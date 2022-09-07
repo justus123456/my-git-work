4f6bc6b49d5f1d31369b17bd63afa0b94a81a269
justus123456 <justusidodo@gmail.com>
2022-09-07 10:38:04 +0100

    hello

---
+++ java/video.js
@@ -0,0 +1,13 @@
+ const bog = document.querySelector('.fa-align-justify');
+ const love = document.querySelector('.front');
+
+
+ bog.addEventListener("click",()=>{
+
+    bog.classList.toggle('fa-times');
+
+    love.classList.toggle('active') 
+    
+    
+    
+ });
