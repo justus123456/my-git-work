27587e605717b832c1600c47ce1ea8c470c12b1f
justus <104899713+justus123456@users.noreply.github.com>
2022-09-29 13:45:30 +0100

    help

---
+++ indexf.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>STORM AIR SHOES,BAGS & CLOTHES VENTURES</title>
+    <link rel="stylesheet" href="css/first.css?v=<?php time(); ?>">
+    <link rel="stylesheet" href="css/header.css?v=<?php time(); ?>">
+    <link rel="stylesheet" href="fonts/css/all.css">
+    <link rel="shortcut icon" href="images/images (8).jpeg">
+</head>
+
+<body>
+    <div class="preloader">
+
+    </div>
+    <?php require "modules/header.php"; ?>
+    <section>
+        <div class="vid">
+            <img src="images/pexels-grafixartphoto-samir-belhamra-4254555.jpg" class="hg" alt="">]
+            <h1>the best wears</h1>
+            <p>get your wears from anywhere, don't delay, get the best cloths at affordable prices</p>
+            <button type="submit">buy now</button>
+        </div>
+    </section>
+    <section>
+        <div class="one">
+            <p >
+                stom air offers the best service in the clothing business, we deal with shoes,clothes all in making sure our customers are satisfied. for more information click on our contact  
+            </p>
+        </div>
+        <div class="bannerrow">
+          <h1>download the storm air app here, for free <button class="btno">download now</button></h1>
+        </div>
+    </section>
+    <section>
+        <div class="pro">
+            <h1>newsletter</h1>
+            <h4>we take our privacy seriously. we will only use your information to manage your account and provide you with services and support your request. for more information about how we use your information please see our <span class="dd"><a href="#">privacy policy</a></span></h4>
+           <div class="grid">
+            <div class="oon">
+                <img src="images/images__8_-removebg-preview.png" alt="">
+                <p>boxpark433 al el wasl road dubai, UAE.</p>
+                <p>po box 2122676</p>
+                <p>ph: +2393990000 +233489384877</p>
+                <p>E: info@stormair.com</p>
+                <i class="fab fa-facebook"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+            </div>
+            <div class="right">
+                <a href="#" class="hop">about us</a>
+                <a href="#" class="hop">contact us</a>
+                <a href="#" class="hop">blog</a>
+            </div>
+           </div>
+        </div>
+    </section>
+</body>
+
+</html>
